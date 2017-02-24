fe76cfb005eeec97bb257db7210dc265b7d1bf7b
96Asch <aschhuang@gmail.com>
2017-02-24 15:52:35 +0100

    almost

--- Scripts/p4.sh
+++ Scripts/p4.sh
@@ -3,5 +3,11 @@ temp="../temp"
 python="../python"
 result="../Result"
 
-bzcat wc_day*.out.bz2 | grep -v '200','3??' | awk '{print $7}' | sed 's%/[^/]*$%/%' | grep -v '\.' | sort | uniq -c
+bzcat wc_day*.out.bz2 | grep -E '200|3??' | awk '{print $7}' | sed 's%/[^/]*$%/%' | egrep -v '\.|\*' | egrep -v '\/\/' | sort | uniq -c | sort -k2 > ../temp/tree_data.txt
+
+chmod +x ./../python/asciiTree.py
+./../python/asciiTree.py
+
+cat ../Result/ascii_tree.txt
+rm ../temp/tree_data.txt
 
--- python/asciiTree.py
+++ python/asciiTree.py
@@ -1,8 +1,45 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+treeArray = []
+value = 10
+prevLine = ""
 
-with open("../temp/directory_list.txt","r") as f:
+# Function to calculate the correction for proper alignment.
+def calcAlignment (value, hits):
+	correction = value - len(hits)
+	return correction
+
+# Function to add nodes to the tree list.
+def appendTreeList(treeArray, hits, rank, dirList, correction, deletion):
+	if deletion < 0:
+		deletion = deletion*-1
+	treeArray.append(hits + ' '*correction + "|   "*(rank - deletion) + "x----"*n + dirList[-deletion] + "/")
+	return
+
+# Main function to split the data from a txt file and properly
+# insert them into an ascii tree.
+outFile = open("../Result/ascii_tree.txt", "w")
+with open("../temp/tree_data.txt","r") as f:
 	for line in f:
-		line = line.rstrip("\n");
-		rank = line.count("/")	 -1
-		print line, rank
+		temp = line.split()
+		hits = temp[0]
+		dirString = temp[1].rstrip()
+		rank = dirString.count('/')
+		prevRank = prevLine.count('/')
+		correct = calcAlignment(value, hits)
+		dirList = dirString.split('/')
+
+		if rank > 1:
+			n = 1
+		else:
+			n = 0
+		if (rank-prevRank) > 1: #if directory has never been visited
+			appendTreeList(treeArray, "0", rank, dirList, correct, 3)		
+		appendTreeList(treeArray, hits, rank, dirList, correct, 2)
+		prevLine = line
+
+	correct = calcAlignment(value, "Hits:")
+	outFile.write("Hits:" + " "*correct + "Directory:\n")
+	for node in treeArray:
+		outFile.write(node)
 f.close()
+			
